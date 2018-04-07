724f02f6ad0afba0231672e898bbd1ed0d9d00da
Zukkari <stanislav.myshkovski@gmail.com>
2018-04-07 13:23:48 +0300

    add welcome message to the landing page close #138

--- src/main/kotlin/cs/ut/ui/controllers/LandingPageController.kt
+++ src/main/kotlin/cs/ut/ui/controllers/LandingPageController.kt
@@ -1,11 +1,13 @@
 package cs.ut.ui.controllers
 
+import cs.ut.configuration.ConfigurationReader
 import cs.ut.util.Page
 import org.zkoss.zk.ui.Component
 import org.zkoss.zk.ui.event.Events
 import org.zkoss.zk.ui.select.SelectorComposer
 import org.zkoss.zk.ui.select.annotation.Wire
 import org.zkoss.zul.Button
+import org.zkoss.zul.Html
 
 /**
  *  Controller that responds for landing page and controls found on that page
@@ -18,9 +20,13 @@ class LandingPageController : SelectorComposer<Component>(), Redirectable {
     @Wire
     private lateinit var existingLog: Button
 
+    @Wire
+    private lateinit var welcomeMessage: Html
+
     override fun doAfterCompose(comp: Component?) {
         super.doAfterCompose(comp)
         wireButtons()
+        appendMessage()
     }
 
     /**
@@ -35,5 +41,10 @@ class LandingPageController : SelectorComposer<Component>(), Redirectable {
         existingLog.addEventListener(Events.ON_CLICK, { _ ->
             setContent(Page.TRAINING.value, page)
         })
+    }
+
+    private fun appendMessage() {
+        val msg = ConfigurationReader.findNode("messages/welcome").valueWithIdentifier("label").value
+        welcomeMessage.content = msg
     }
 }
